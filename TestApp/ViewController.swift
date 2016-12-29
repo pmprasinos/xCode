4ae6677afe98af5d675df913425512fdd68c14a5
Phillip Prasinos <phillipprasinos@phillips-mbp.mshome.net>
2016-12-29 01:56:58 -0800

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Phillip Prasinos on 12/29/16.
+//  Copyright © 2016 Phillip Prasinos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
